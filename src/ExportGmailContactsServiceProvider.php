6681c3ffbdf5a9d66aa23bfe3ee124dc7937f1d7
minhajul <kmsohelinfo@gmail.com>
2021-01-26 22:11:18 +0600

    Updated

--- src/ExportGmailContactsServiceProvider.php
+++ src/ExportGmailContactsServiceProvider.php
@@ -9,7 +9,14 @@ class ExportGmailContactsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/export-gmail-contacts.php' => config_path('courier.php'),
+            __DIR__.'/../config/export-gmail-contacts.php' => config_path('export-gmail-contacts.php'),
         ]);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/export-gmail-contacts.php', 'export-gmail-contacts'
+        );
     }
 }
