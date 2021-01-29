b68d57952eeeab528dfd0d143f88fb7d61f14e6d
minhajul <kmsohelinfo@gmail.com>
2021-01-29 09:46:41 +0600

    Fixed routing namespace

--- src/ExportGmailContactsServiceProvider.php
+++ src/ExportGmailContactsServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Minhajul\ExportGmailContacts;
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class ExportGmailContactsServiceProvider extends ServiceProvider
